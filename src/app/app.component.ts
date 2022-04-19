7f4a9ef83bcb6f24686ccec36a3f37dc702979c0
shashankc-systango <schoudhary@isystango.com>
2022-04-19 16:29:09 +0530

    feat: added a demo project

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+
+export class AppComponent  {
+  points = [];
+  signatureImage;
+
+  showImage(data) {
+    this.signatureImage = data;
+  }
+}
+ssssss
